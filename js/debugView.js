016e206facb03b251fceee091c844a34e2ee2176
GuilhermeC <guiguimcouto@gmail.com>
2022-06-07 17:50:57 -0300

    Mudanças e ajustes feitos para funcionamento linear

--- js/debugView.js
+++ js/debugView.js
@@ -4,17 +4,19 @@ function nextFrame() {
 
     fetch("../debug.json").then(response => response.json()).then(data => {
 
+        const previousView = document.querySelector("#previousView");
+        const currentView = document.querySelector("#currentView");
+        const frameView = document.querySelector("#frameView");
+        previousView.innerHTML = currentView.innerHTML;
+        currentView.innerHTML = frameView.innerHTML;
+
+        stepCounter = stepCounter + 1;
+
         if (stepCounter <= data.debugFrames.length) {
-            const previousView = document.querySelector("#previousView");
-            const currentView = document.querySelector("#currentView");
-            const frameView = document.querySelector("#frameView"); // the debugView element on html
+
             const frame = data.debugFrames[stepCounter]; // the step frame from json
 
-            previousView.innerHTML = currentView.innerHTML;
-            currentView.innerHTML = frameView.innerHTML;
-
             if (frame.previousPEG == null) {  // if the frame represents the rule's first call
-
                 if (frame.tag === "i") {
                     frameView.innerHTML = `
                     <li>
@@ -39,7 +41,6 @@ function nextFrame() {
             }
 
             else {  // if the frame doesn't represent the rule's first call
-
                 if (frame.tag === "i") {
                     frameView.innerHTML = `
                     <li>
@@ -52,7 +53,6 @@ function nextFrame() {
                     </li>
                     `;
                 }
-
                 else if (frame.tag === "s") {
                     frameView.innerHTML = `
                     <li>
@@ -65,7 +65,6 @@ function nextFrame() {
                     </li>
                     `;
                 }
-
                 else {
                     frameView.innerHTML = `
                     <li>
@@ -80,13 +79,15 @@ function nextFrame() {
                 }
             }
 
-            // input
-            const currentFrame = data.debugFrames[stepCounter - 1];
+            // input and status
             const inputView = document.querySelector("#inputView");
-            const inputIndex = currentFrame.input;
-            const userInput = data.userInput;
-
+            const statusView = document.querySelector("#statusView");
             inputView.innerHTML = ``;
+            statusView.innerHTML = ``;
+
+            const currentFrame = data.debugFrames[stepCounter - 1];
+            const userInput = data.userInput;
+            const inputIndex = currentFrame.input;
 
             if (userInput.length <= 5) {
                 for (let i = 0; i < userInput.length; i++) {
@@ -99,281 +100,77 @@ function nextFrame() {
                 }
             }
 
-            // status
-            const statusView = document.querySelector("#statusView");
+            // precisa completar com as demais condições para quando for maior que 5 !!!!!!!!!
+
             const status = currentFrame.status;
             statusView.innerHTML = `${status}`;
-
         }
-
-        /* ANTIGO
-        if (stepCounter <= data.debugFrames.length) {
-            const debugView = document.querySelector("#debugView"); // the debugView element on html
-            const frame = data.debugFrames[stepCounter]; // the step frame from json
-
-            console.log(frame);
-
-            if (frame.previousPEG == null) {  // if the frame represents the rule's first call
-
-                if (frame.tag === "i") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret dv-item"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-
-                else if (frame.tag === "s") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret dv-item" style="color:green;"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-
-                else {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret dv-item" style="color:red;"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-            }
-
-            else {  // if the frame doesn't represent the rule's first call
-
-                if (frame.tag === "i") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-
-                else if (frame.tag === "s") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item" style="color:green;">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-
-                else {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item" style="color:red;">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-            }
-            
-            // input
-            const userInput = data.userInput;
-            const inputLine = document.querySelector("#inputLine").cells;
-            const inputIndex = frame.input;
-
-            for(let i = 0; i <= userInput.length; i ++) {
-                const pointerPos = document.querySelector(`#pointer${i}`);
-                console.log(pointerPos);
-
-                if(i === inputIndex) {
-                    pointerPos.innerHTML = `^`;
-                    inputLine[i].setAttribute("style", "border-color:orangered;");
-                } else if(i === userInput.length && i === inputIndex) {
-                    pointerPos.innerHTML = `^`;
-                    inputLine[i].setAttribute("style", "border-color:orangered;");
-                } else {
-                    pointerPos.innerHTML = ``;
-                    inputLine[i].setAttribute("style", "border-color:rgb(233,233,233);");
-                }
-            }
-
-            // depth
-            const depthHeap = document.querySelector("#depthHeap");
-            depthHeap.innerHTML = `<tr><td></td></tr>`; // clear depth table
-            const depth = frame.callStack;
-
-            if(depth.length) {
-                for(let i = depth.length - 1; i >= 0; i--) {
-                    depthHeap.innerHTML += `
-                    <tr><td>${depth[i]}</td></tr>
-                    `;
-                }
-            } 
-
-            // status
-            const status = document.querySelector("#status");
-            status.innerHTML += `${frame.status}\n`;
-            status.scrollTop = status.scrollHeight;
-
-        }
-
-        */
     });
-    stepCounter = stepCounter + 1;
 }
 
+
+// PREVIOUS
 function previousFrame() {
-    stepCounter = stepCounter - 1;
 
     fetch("../debug.json").then(response => response.json()).then(data => {
 
-        if (stepCounter <= data.debugFrames.length) {
+        if (stepCounter - 1 >= 0) {
+            stepCounter = stepCounter - 1;
+
             const previousView = document.querySelector("#previousView");
             const currentView = document.querySelector("#currentView");
-            const frameView = document.querySelector("#frameView"); // the debugView element on html
-            const frame = data.debugFrames[stepCounter]; // the step frame from json
+            const frameView = document.querySelector("#frameView");
 
-            frameView.innerHTML = currentView.innerHTML;
-            currentView.innerHTML = previousView.innerHTML;
+            const inputView = document.querySelector("#inputView");
+            const statusView = document.querySelector("#statusView");
+            inputView.innerHTML = ``;
+            statusView.innerHTML = ``;
 
-            if (frame.previousPEG == null) {  // if the frame represents the rule's first call
+            if (stepCounter === 0) {
+                frameView.innerHTML = currentView.innerHTML;
+                currentView.innerHTML = ``;
+            }
+            else if (stepCounter === 1) {
+                frameView.innerHTML = currentView.innerHTML;
+                currentView.innerHTML = previousView.innerHTML;
+                previousView.innerHTML = ``;
+            }
+            else {
+                frameView.innerHTML = currentView.innerHTML;
+                currentView.innerHTML = previousView.innerHTML;
+            }
 
-                if (frame.tag === "i") {
-                    previousView.innerHTML = `
+            if (stepCounter <= data.debugFrames.length && stepCounter >= 2) {
+
+                const frame = data.debugFrames[stepCounter - 2]; // the step frame from json
+
+                if (frame.previousPEG == null) {  // if the frame represents the rule's first call
+                    if (frame.tag === "i") {
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret"><strong class="dv-item">${frame.actualPEG}</strong></span>
                     </li>
                     `;
-                }
-
-                else if (frame.tag === "s") {
-                    previousView.innerHTML = `
+                    }
+                    else if (frame.tag === "s") {
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret" style="color:green;"><strong class="dv-item">${frame.actualPEG}</strong></span>
                     </li>
                     `;
-                }
-
-                else {
-                    previousView.innerHTML = `
-                    <li>
-                        <span class="caret" style="color:red;"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-            }
-
-            else {  // if the frame doesn't represent the rule's first call
-
-                if (frame.tag === "i") {
-                    previousView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-
-                else if (frame.tag === "s") {
-                    previousView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item" style="color:green;">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-
-                else {
-                    previousView.innerHTML = `
-                    <li>
-                        <span class="caret-down dv-item">${frame.previousPEG}</span>
-                        <ul class="active">
-                            <li class="dv-item" style="color:red;">
-                                <strong class="dv-item">${frame.actualPEG}</strong>
-                            </li>
-                        </ul>
-                    </li>
-                    `;
-                }
-            }
-
-            // input
-            const currentFrame = data.debugFrames[stepCounter - 1];
-            const inputView = document.querySelector("#inputView");
-            const inputIndex = currentFrame.input;
-            const userInput = data.userInput;
-
-            inputView.innerHTML = ``;
-
-            if (userInput.length <= 5) {
-                for (let i = 0; i < userInput.length; i++) {
-                    if (i === inputIndex) {
-                        inputView.innerHTML += `<span style="color:orangered;">${userInput[i]}</span>`;
                     }
                     else {
-                        inputView.innerHTML += `${userInput[i]}`;
-                    }
-                }
-            }
-
-            // status
-            const statusView = document.querySelector("#statusView");
-            const status = currentFrame.status;
-            statusView.innerHTML = `${status}`;
-        }
-
-        /* ANTIGO
-        if (stepCounter <= data.debugFrames.length) {
-            const debugView = document.querySelector("#debugView"); // the debugView element on html
-            const frame = data.debugFrames[stepCounter]; // the step frame from json
-
-            console.log(frame);
-
-            if (frame.previousPEG == null) {  // if the frame represents the rule's first call
-
-                if (frame.tag === "i") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-
-                else if (frame.tag === "s") {
-                    debugView.innerHTML = `
-                    <li>
-                        <span class="caret" style="color:green;"><strong class="dv-item">${frame.actualPEG}</strong></span>
-                    </li>
-                    `;
-                }
-
-                else {
-                    debugView.innerHTML = `
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret" style="color:red;"><strong class="dv-item">${frame.actualPEG}</strong></span>
                     </li>
                     `;
+                    }
                 }
-            }
 
-            else {  // if the frame doesn't represent the rule's first call
-
-                if (frame.tag === "i") {
-                    debugView.innerHTML = `
+                else {  // if the frame doesn't represent the rule's first call
+                    if (frame.tag === "i") {
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret-down dv-item">${frame.previousPEG}</span>
                         <ul class="active">
@@ -383,10 +180,9 @@ function previousFrame() {
                         </ul>
                     </li>
                     `;
-                }
-
-                else if (frame.tag === "s") {
-                    debugView.innerHTML = `
+                    }
+                    else if (frame.tag === "s") {
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret-down dv-item">${frame.previousPEG}</span>
                         <ul class="active">
@@ -396,10 +192,9 @@ function previousFrame() {
                         </ul>
                     </li>
                     `;
-                }
-
-                else {
-                    debugView.innerHTML = `
+                    }
+                    else {
+                        previousView.innerHTML = `
                     <li>
                         <span class="caret-down dv-item">${frame.previousPEG}</span>
                         <ul class="active">
@@ -409,48 +204,33 @@ function previousFrame() {
                         </ul>
                     </li>
                     `;
+                    }
                 }
+
             }
 
-            // input
-            const userInput = data.userInput;
-            const inputLine = document.querySelector("#inputLine").cells;
-            const inputIndex = frame.input;
+            // input and status
+            if (stepCounter > 0) {
+                const currentFrame = data.debugFrames[stepCounter - 1];
+                const userInput = data.userInput;
+                const inputIndex = currentFrame.input;
 
-            for(let i = 0; i < userInput.length; i ++) {
-                const pointerPos = document.querySelector(`#pointer${i}`);
-                console.log(pointerPos);
-
-                if(i === inputIndex) {
-                    pointerPos.innerHTML = `^`;
-                    inputLine[i].setAttribute("style", "border-color:orangered;");
-                } else if(i === userInput.length && i === inputIndex) {
-                    pointerPos.innerHTML = `^`;
-                    inputLine[i].setAttribute("style", "border-color:orangered;");
-                } else {
-                    pointerPos.innerHTML = ``;
-                    inputLine[i].setAttribute("style", "border-color:rgb(233,233,233);");
+                if (userInput.length <= 5) {
+                    for (let i = 0; i < userInput.length; i++) {
+                        if (i === inputIndex) {
+                            inputView.innerHTML += `<span style="color:orangered;">${userInput[i]}</span>`;
+                        }
+                        else {
+                            inputView.innerHTML += `${userInput[i]}`;
+                        }
+                    }
                 }
+
+                // precisa completar com as demais condições para quando for maior que 5 !!!!!!!!!
+
+                const status = currentFrame.status;
+                statusView.innerHTML = `${status}`;
             }
-
-            // depth
-            const depthHeap = document.querySelector("#depthHeap");
-            depthHeap.innerHTML = `<tr><td></td></tr>`; // clear depth table
-            const depth = frame.callStack;
-
-            if(depth.length) {
-                for(let i = depth.length - 1; i >= 0; i--) {
-                    depthHeap.innerHTML += `
-                    <tr><td>${depth[i]}</td></tr>
-                    `;
-                }
-            } 
-
-            // status
-            const status = document.querySelector("#status");
-            status.innerHTML += `${frame.status}\n`;
-            status.scrollTop = status.scrollHeight;
         }
-        */
     });
 }
