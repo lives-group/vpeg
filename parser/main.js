4fde36299377fb970959b9ccc1e92ed423e3665e
lucasvarino <lucasvarino@gmail.com>
2023-10-10 14:56:57 -0300

    Base do Visitor pronta

--- parser/main.js
+++ parser/main.js
@@ -40,23 +40,92 @@ class CustomErrorListener extends antlr4.ErrorListener {
     }
 }
 
-class Visitor {
-    visitChildren(ctx) {
-      if (!ctx) {
-        return;
-      }
-  
-      if (ctx.children) {
-        return ctx.children.map(child => {
-          if (child.children && child.children.length != 0) {
+class Visitor extends antlr4.ParseTreeVisitor {
+  constructor(tokens) {
+    super(tokens);
+  }
+
+  visitProduction(ctx) {
+    const ruleName = ctx.ID().getText();
+    const expression = this.visit(ctx.peg_expr());
+    return { rule: ruleName, expression };
+  }
+
+  visitPeg_expr(ctx) {
+    if (ctx.peg_seq()) {
+      return this.visit(ctx.peg_seq());
+    } else {
+      const left = this.visit(ctx.peg_seq(0));
+      const right = this.visit(ctx.peg_expr());
+      return { operator: '/', left, right };
+    }
+  }
+
+  visitPeg_seq(ctx) {
+    const prefixes = ctx.peg_prefix().map(prefix => this.visit(prefix));
+    return prefixes;
+  }
+
+  visitPeg_prefix(ctx) {
+    if (ctx.AMPERSAND()) {
+      const expression = this.visit(ctx.peg_unary_op());
+      return { operator: '&', expression };
+    } else if (ctx.EXCLAMATION()) {
+      const expression = this.visit(ctx.peg_unary_op());
+      return { operator: '!', expression };
+    } else {
+      return this.visit(ctx.peg_unary_op());
+    }
+  }
+
+  visitPeg_unary_op(ctx) {
+    const factor = this.visit(ctx.peg_factor());
+    if (ctx.QUESTION()) {
+      return { operator: '?', expression: factor };
+    } else if (ctx.STAR()) {
+      return { operator: '*', expression: factor };
+    } else if (ctx.PLUS()) {
+      return { operator: '+', expression: factor };
+    } else {
+      return factor;
+    }
+  }
+
+  visitPeg_factor(ctx) {
+    if (ctx.STRING_LITERAL()) {
+      return ctx.STRING_LITERAL().getText();
+    } else if (ctx.ID()) {
+      return ctx.ID().getText();
+    } else if (ctx.range()) {
+      return this.visit(ctx.range());
+    } else if (ctx.LAMBDA()) {
+      return 'empty';
+    } else if (ctx.peg_expr()) {
+      return this.visit(ctx.peg_expr());
+    } else if (ctx.CHARACTER_CLASS()) {
+      return ctx.CHARACTER_CLASS().getText();
+    } else {
+      return ctx.RANGE_LITERAL().getText();
+    }
+  }
+
+  visitRange(ctx) {
+    const start = ctx.RANGE_CHAR(0).getText();
+    const end = ctx.RANGE_CHAR(1).getText();
+    return { type: 'range', start, end };
+  }
+
+  visit(ctx) {
+    if (Array.isArray(ctx)) {
+        return ctx.map(function(child) {
             return child.accept(this);
-          } else {
-            return child.getText();
-          }
-        });
-      }
+        }, this);
+    } else {
+        return ctx.accept(this);
     }
 }
+}
+
 
 function verifyGrammar() {
     let text = input.textContent;
@@ -71,6 +140,10 @@ function verifyGrammar() {
     parser.addErrorListener(errorListener); // Anexa nosso ouvinte de erro personalizado
 
     const tree = parser.rules();
+    const visitor = new Visitor();
+    const parsedData = visitor.visit(tree.children[0]);
+    console.log(parsedData);
+
     
 
     if (parser.syntaxErrorsCount > 0) {
